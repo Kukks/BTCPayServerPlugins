b8d244380f1c4fc3debd73d1b27a0137cc734a09
Kukks <evilkukka@gmail.com>
2023-06-19 08:54:10 +0200

    Add File Seller plugin

---
+++ Plugins/BTCPayServer.Plugins.FileSeller/FileSellerPlugin.cs
@@ -0,0 +1,27 @@
+﻿using BTCPayServer.Abstractions.Contracts;
+using BTCPayServer.Abstractions.Models;
+using BTCPayServer.Abstractions.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BTCPayServer.Plugins.FileSeller;
+
+public class FileSellerPlugin : BaseBTCPayServerPlugin
+{
+    public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
+    {
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=1.10.3" }
+    };
+    public override void Execute(IServiceCollection applicationBuilder)
+    {
+        applicationBuilder.AddHostedService<FileSellerService>();
+        applicationBuilder.AddSingleton<IUIExtension>(new UIExtension("FileSeller/Detect",
+            "header-nav"));
+        applicationBuilder.AddSingleton<IUIExtension>(new UIExtension("FileSeller/Detect",
+            "checkout-end"));
+        applicationBuilder.AddSingleton<IUIExtension>(new UIExtension("FileSeller/FileSellerTemplateEditorItemDetail",
+            "app-template-editor-item-detail"));
+        
+
+        base.Execute(applicationBuilder);
+    }
+}
---
+++ Plugins/BTCPayServer.Plugins.FileSeller/README.md
@@ -0,0 +1,17 @@
+﻿# File Seller Plugin
+
+This plugin allows you to sell files on your BTCPay Server.
+
+## Installation
+
+1. Go to the "Plugins" page of your BTCPay Server
+2. Click on install under the "File Seller" plugin listing
+3. Restart BTCPay Server
+4. Go to Server Settings => Files
+5. Ensure that there is a file storage provider configured
+6. Click on "Add File"
+7. Go to your point of sale or crowdfund app settings
+8. Click on "Edit" on the item/perk you'd like to provide a file upon purchase.
+9. Select the file you'd like to provide from the new dropdown list titled "File"
+10. Close the editor and save the app
+11. Upon purchase (invoice marked as settled), a new section will appear on the invoice receipt with a link for each file unlocked.
