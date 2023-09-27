c1d2842ce115d6f3a6c02fcae2ba72a6736b03ac
Kukks <evilkukka@gmail.com>
2023-09-27 08:11:52 +0200

    remove nfc plugin

--- Plugins/BTCPayServer.Plugins.NFC/Pack.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish -c Altcoins-Release -o bin/publish/BTCPayServer.Plugins.NFC
-dotnet run -p ../../BTCPayServer.PluginPacker bin/publish/BTCPayServer.Plugins.NFC BTCPayServer.Plugins.NFC ../packed
