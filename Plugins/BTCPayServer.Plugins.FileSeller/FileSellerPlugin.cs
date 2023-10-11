8fda80315834af8b76de5c285e6eb1927fba6099
Kukks <evilkukka@gmail.com>
2023-10-11 08:25:16 +0200

    update fs plugin

--- Plugins/BTCPayServer.Plugins.FileSeller/FileSellerPlugin.cs
+++ Plugins/BTCPayServer.Plugins.FileSeller/FileSellerPlugin.cs
@@ -9,7 +9,7 @@ public class FileSellerPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=1.11.4" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=1.11.5" }
     };
     public override void Execute(IServiceCollection applicationBuilder)
     {
