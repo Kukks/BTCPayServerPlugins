9e8d694209e109c8bba73acf6c401a41c76f00fb
Kukks <evilkukka@gmail.com>
2023-11-02 10:13:42 +0100

    Remove AOPP

--- Plugins/BTCPayServer.Plugins.AOPP/Pack.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish -c Altcoins-Release -o bin/publish/BTCPayServer.Plugins.AOPP
-dotnet run -p ../../BTCPayServer.PluginPacker bin/publish/BTCPayServer.Plugins.AOPP BTCPayServer.Plugins.AOPP ../packed
--- Plugins/BTCPayServer.Plugins.AOPP/UpdateAOPPSettingsViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BTCPayServer.Plugins.AOPP
-{
-    public class UpdateAOPPSettingsViewModel
-    {
-        public bool Enabled { get; set; }
-        public string StoreName { get; set; }
-    }
-}
