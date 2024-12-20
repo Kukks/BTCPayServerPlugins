cdadf6fa76d49f6a3e141d5e542204a43e7f5a25
TowyTowy <85077986+TowyTowy@users.noreply.github.com>
2024-12-20 07:57:49 +0100

    Update FixedFloatPlugin.cs
    Fix version

--- Plugins/BTCPayServer.Plugins.FixedFloat/FixedFloatPlugin.cs
+++ Plugins/BTCPayServer.Plugins.FixedFloat/FixedFloatPlugin.cs
@@ -9,7 +9,7 @@ namespace BTCPayServer.Plugins.FixedFloat
     {
         public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
         {
-            new() { Identifier = nameof(BTCPayServer), Condition = ">2.0.4" }
+            new() { Identifier = nameof(BTCPayServer), Condition = ">=2.0.4" }
 
         };
         public override void Execute(IServiceCollection applicationBuilder)
