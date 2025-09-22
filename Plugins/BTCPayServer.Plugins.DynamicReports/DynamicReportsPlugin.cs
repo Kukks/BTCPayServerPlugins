d27c3d5629f4ede5e1fdf34003616b2096090652
Andrew Camilleri (Kukks) <evilkukka@gmail.com>
2025-09-22 14:05:21 +0200

    bumping dyn rreports plugin

--- Plugins/BTCPayServer.Plugins.DynamicReports/DynamicReportsPlugin.cs
+++ Plugins/BTCPayServer.Plugins.DynamicReports/DynamicReportsPlugin.cs
@@ -10,7 +10,7 @@ public class DynamicReportsPlugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.0.0" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.2.0" }
     };
     public override void Execute(IServiceCollection applicationBuilder)
     {
