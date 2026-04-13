c1894cf2271dd3375030d7d34bad80b404bf0121
baptizer <johnfawole18@gmail.com>
2026-04-13 19:43:08 +0100

    Remove Wabisabi plugin files including settings, wallet provider, and coordinator status fetcher. Delete associated documentation and images.

--- Plugins/BTCPayServer.Plugins.Wabisabi/Pack.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish BTCPayServer.Plugins.Wabisabi.csproj -c Release -o bin/publish/BTCPayServer.Plugins.Wabisabi
-dotnet run -p ../../submodules/btcpayserver/BTCPayServer.PluginPacker bin/publish/BTCPayServer.Plugins.Wabisabi BTCPayServer.Plugins.Wabisabi ../packed
