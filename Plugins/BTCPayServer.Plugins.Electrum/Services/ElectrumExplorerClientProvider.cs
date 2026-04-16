7789bb028edc30dfd2ac69a0f20669e3f5d2ca84
Andrew Camilleri <evilkukka@gmail.com>
2026-04-16 06:53:44 +0200

    refactor: Remove reflection hacks from ElectrumExplorerClientProvider
    Leverage upstream btcpayserver/btcpayserver#7308 which added a protected
    constructor and protected _Clients to ExplorerClientProvider. Removes
    reflection, NullHttpClientFactory, and dummy constructor arguments.

--- Plugins/BTCPayServer.Plugins.Electrum/Services/ElectrumExplorerClientProvider.cs
+++ Plugins/BTCPayServer.Plugins.Electrum/Services/ElectrumExplorerClientProvider.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection;
 using BTCPayServer.HostedServices;
 using NBXplorer;
 
@@ -12,10 +10,6 @@ namespace BTCPayServer.Plugins.Electrum.Services;
 /// Replaces ExplorerClientProvider. Creates ExplorerClient instances backed by
 /// ElectrumHttpHandler, so all NBXplorer HTTP calls are intercepted and routed
 /// to our Electrum engine.
-///
-/// Since ExplorerClientProvider's methods (GetExplorerClient, IsAvailable, GetAll)
-/// are NOT virtual, we can't override them. Instead, we populate the base class's
-/// private _Clients dictionary via reflection so the base methods work correctly.
 /// </summary>
 public class ElectrumExplorerClientProvider : ExplorerClientProvider
 {
@@ -23,13 +17,8 @@ public class ElectrumExplorerClientProvider : ExplorerClientProvider
         BTCPayNetworkProvider networkProvider,
         NBXplorerDashboard dashboard,
         ElectrumHttpHandler handler)
-        : base(new NullHttpClientFactory(), networkProvider, CreateEmptyOptions(), dashboard, CreateEmptyLogs())
+        : base(networkProvider, dashboard)
     {
-        // Access the base class's private _Clients dictionary
-        var clientsField = typeof(ExplorerClientProvider)
-            .GetField("_Clients", BindingFlags.NonPublic | BindingFlags.Instance);
-        var clients = (Dictionary<string, ExplorerClient>)clientsField!.GetValue(this);
-
         foreach (var network in networkProvider.GetAll().OfType<BTCPayNetwork>())
         {
             var httpClient = new HttpClient(handler, disposeHandler: false)
@@ -43,22 +32,7 @@ public class ElectrumExplorerClientProvider : ExplorerClientProvider
             explorerClient.SetClient(httpClient);
             explorerClient.SetNoAuth();
 
-            clients[network.CryptoCode.ToUpperInvariant()] = explorerClient;
+            _Clients[network.CryptoCode.ToUpperInvariant()] = explorerClient;
         }
-    }
-
-    private static Microsoft.Extensions.Options.IOptions<BTCPayServer.Configuration.NBXplorerOptions> CreateEmptyOptions()
-    {
-        return Microsoft.Extensions.Options.Options.Create(new BTCPayServer.Configuration.NBXplorerOptions());
-    }
-
-    private static BTCPayServer.Logging.Logs CreateEmptyLogs()
-    {
-        return new BTCPayServer.Logging.Logs();
-    }
-
-    private class NullHttpClientFactory : IHttpClientFactory
-    {
-        public HttpClient CreateClient(string name) => new HttpClient();
     }
 }
